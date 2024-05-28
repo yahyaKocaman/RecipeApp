fdc7df6a5094ce12efdf993aed6e5b29f997949d
yahyaKocaman <yahyakocaman28@gmail.com>
2024-05-28 10:08:09 +0300

    unnecessary cards deleted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.t.recipeapp"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
